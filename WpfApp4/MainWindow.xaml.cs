5215d7d493a686c11ce9bac46492bdd1172f5348
Hubber773 <githubbr53@gmail.com>
2025-03-11 11:27:26 +0100

    DZIAŁAAAAAAA HASŁO DZIAŁA

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -20,5 +20,29 @@ namespace WpfApp4
         {
             InitializeComponent();
         }
+
+        private void B_ginner_Click(object sender, RoutedEventArgs e)
+        {
+            int ileZnaków = TB_znaki.Text.Length;
+            //char hasztag = '#'
+            string[] tabliczka = new string[ileZnaków];
+            Random rnd = new Random();
+
+            string litery = "qwertyuiopasdfghjklzxcvbnnnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            string cyfry = "1234567890";
+            string specjalne = "!@#$%^&*-=_+";
+            string haslo = "";
+
+            for (int i = 0; i < tabliczka.Length; i++)
+            {
+                if (CB_litery.IsChecked == true)
+                {
+                    //MessageBox.Show($"{ileZnaków}");
+                    tabliczka[0] = litery[rnd.Next(0, litery.Length)].ToString();
+                    haslo += tabliczka[0];
+                }
+            }
+            MessageBox.Show(haslo);
+        }
     }
 }
